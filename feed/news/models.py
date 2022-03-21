204b19199393c8350a9299484d01e941b8967d83
Ivan Myshlyaev <ivanm1989@ya.ru>
2022-03-21 21:42:31 +0700

    edited models and admin

--- feed/news/models.py
+++ feed/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=250)
+
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    body = models.TextField()
+
+    image = models.ImageField()
