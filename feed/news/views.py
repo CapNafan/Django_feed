bfb649e2cc5f3e798e8d8fe93ad77bfcb8e00616
Ivan Myshlyaev <ivanm1989@ya.ru>
2022-04-10 13:19:39 +0700

    added pagination and number of news per page setting

--- feed/news/views.py
+++ feed/news/views.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 from .models import NewsItem
 from .forms import NewsForm
 from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def list_news(request):
+    per_page = request.GET.get('per_page', 10)
     items = NewsItem.objects.order_by("-publish_date")
-    return render(request, 'news/list.html', context={'items': items})
+    paginator = Paginator(items, per_page)
+    page_number = request.GET.get('page')
+
+    try:
+        paginated = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        paginated = paginator.get_page(1)
+    except EmptyPage:
+        paginated = paginator.page(paginator.num_pages)
+
+    return render(request, 'news/list.html', context={'items': paginated, 'per_page': per_page})
 
 
 def add_news(request):
